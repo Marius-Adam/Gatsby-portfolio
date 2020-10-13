9d7516ca59acf8e91a7081e2f5cb15a9fbb1379f
Marius-Adam <marius.c.adam@gmail.com>
2020-10-13 14:33:18 +0100

    Updated OG image

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,8 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // Of course you can also add new values here to query them like usual
     siteTitleAlt: `MA Portfolio Website`,
+    // Used for og:image and must be placed inside the `static` folder
+    siteImage: `/banner.jpg`,
   },
   plugins: [
     {
